4ba6a6e533070020417f2dcaa059153be9ba2be8
manto-png <mikerantonelli@gmail.com>
2025-02-18 14:50:13 -0500

    updates config json

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'manto-png', // Usually your GitHub org/user name.
   projectName: 'manto-png.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages'
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
